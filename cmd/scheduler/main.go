4c0ba9cf6b017a7c271d6e541f1a073dc4bcc387
Benyamin <benyaminmahmoudyan@gmail.com>
2024-03-06 15:34:30 +0330

    fix: cmd scheduler

--- cmd/scheduler/main.go
+++ cmd/scheduler/main.go
@@ -2,17 +2,34 @@ package main
 
 import (
 	"fmt"
+	presenceClient "gameapp/adapter/presence"
+	"gameapp/adapter/redis"
+	"gameapp/config"
+	"gameapp/repository/redis/redismatching"
 	"gameapp/scheduler"
+	"gameapp/service/matchingservice"
 	"os"
 	"os/signal"
+	"sync"
 )
 
 func main() {
+	cfg := config.Load("config.yml")
+
+	redisAdapter := redis.New(cfg.Redis)
+	matchingRepo := redismatching.New(redisAdapter)
+	presenceAdapter := presenceClient.New(":8086")
+
+	matchingSvc := matchingservice.New(cfg.MatchingService, matchingRepo, presenceAdapter, redisAdapter)
+
 	done := make(chan bool)
+	var wg sync.WaitGroup
 
 	go func() {
-		sch := scheduler.New()
-		sch.Start(done)
+		sch := scheduler.New(matchingSvc, cfg.Scheduler)
+
+		wg.Add(1)
+		sch.Start(done, &wg)
 	}()
 
 	quit := make(chan os.Signal, 1)
