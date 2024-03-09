0bc86b8d95996e2df136e3dc0ded0905fe7f6a74
Benyamin <benyaminmahmoudyan@gmail.com>
2024-03-09 23:27:01 +0330

    fix richerror

--- pkg/richerror/richerror.go
+++ pkg/richerror/richerror.go
@@ -49,7 +49,7 @@ func (r RichError) WithMeta(meta map[string]interface{}) RichError {
 }
 
 func (r RichError) Error() string {
-	if r.message == "" {
+	if r.message == "" && r.wrappedError != nil {
 		return r.wrappedError.Error()
 	}
 	return r.message
@@ -74,9 +74,13 @@ func (r RichError) Message() string {
 	}
 
 	re, ok := r.wrappedError.(RichError)
-	if !ok {
+	if ok {
+		return re.Message()
+	}
+
+	if r.wrappedError != nil {
 		return r.wrappedError.Error()
 	}
 
-	return re.Message()
+	return ""
 }
